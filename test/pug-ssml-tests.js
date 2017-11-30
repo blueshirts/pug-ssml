00a20012a1fa241e2b098814f95207dcc30ee795
blueshirts <milanoa@yahoo.com>
2017-11-29 23:42:26 -0500

    SSML mixin updates.
    - Added additional SSML mixins.
    - Refactored and added additional test cases.
    - Updated documentation.

--- test/pug-ssml-tests.js
+++ test/pug-ssml-tests.js
@@ -5,7 +5,7 @@ const chai = require('chai')
 const should = chai.should()
 
 const ssml = require('../index')
-const templates = './templates'
+const templatesPath = './templates'
 
 describe('pug-ssml-tests', function() {
   describe('#precompile()', function() {
@@ -13,58 +13,68 @@ describe('pug-ssml-tests', function() {
     const output = './dist'
     const module = `${output}/${file}`
     const importName = `../${output}/${file}`
+    let templates
 
-    beforeEach(function() {
+    before(function() {
       // Delete the existing module.
       if (fs.existsSync(module)) {
         fs.unlinkSync(module)
       }
 
       // Precompile the template.
-      ssml.precompile(templates, {
+      ssml.precompile(templatesPath, {
         file: file,
         output: output
       })
-    })
 
-    it('should generate a node module', function() {
-      should.exist(fs.existsSync(module), `${module} should exist`)
-    })
-    it('should import the new module', function() {
-      const templates = require(importName)
+      // Require the module.
+      templates = require(importName)
       should.exist(templates)
-      should.exist(templates.sentence)
-      should.exist(templates.using_mixin)
     })
-    it('execute a variety of templates', function() {
-      const templates = require(importName)
-      let result
 
-      // sentence
-      result = templates.sentence()
+    it('should render s', function() {
+      should.exist(templates.sentence)
+      const result = templates.sentence()
       should.exist(result)
       result.should.equal('<s>This is a simple speech sentence.</s>')
-
-      // using_mixin
-      result = templates.using_mixin()
+    })
+    it('should render a mixin', function() {
+      should.exist(templates.using_mixin)
+      const result = templates.using_mixin()
       should.exist(result)
       result.should.equal('<s>This is a test of a template that is<prosody volume="loud">using a mixin!</prosody></s>')
-
-      // paragraphs
-      result = templates.paragraphs()
+    })
+    it('should render a p', function() {
+      should.exist(templates.paragraphs())
+      const result = templates.paragraphs()
       should.exist(result)
       result.should.equal('<p>This is paragraph one.</p><p>This is paragraph two.</p><p>This is paragraph three.</p>')
-
-      // using variables
-      result = templates.using_variables({variable: true})
+    })
+    it('should render variables', function() {
+      should.exist(templates.using_variables)
+      const result = templates.using_variables({variable: true})
       should.exist(result)
       result.should.equal('<s>This is a template using a variable here: true</s>')
-
-      // using helper
-      result = templates.using_helper({'helper': () => { return true } })
+    })
+    it('should render a helper', function() {
+      should.exist(templates.using_helper)
+      const result = templates.using_helper({'helper': () => { return true } })
       should.exist(result)
       result.should.equal('<s>This is a template using a helper here: true</s>')
     })
+    it('should render whisper', function() {
+      should.exist(templates.whispered)
+      const result = templates.whispered()
+      should.exist(result)
+      result.should.equal('<s><amazon:effect name="whispered">This is a whispered sentence.</amazon:effect></s>')
+    })
+    it('should render weak-break', function() {
+      should.exist(templates.weak_break)
+      const result = templates.weak_break()
+      should.exist(result)
+      result.should.equal('<s><break strength="weak">This is a weak sentence.</break></s>')
+    })
 
   })
+
 })
